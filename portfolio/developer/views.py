b4035446f6470b869799b2097d9fea97758e6cf0
MosesGich <mosemuthoni57@gmail.com>
2025-10-03 09:54:18 +0300

    Adding Bio page

--- portfolio/developer/views.py
+++ portfolio/developer/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "developer/home.html")
+
+def about(request):
+    return render(request,"developer/about.html")
