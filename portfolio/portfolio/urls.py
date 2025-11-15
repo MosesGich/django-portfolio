219d4ff5991fb4ef515d396b09ddd9b68550ea96
MosesGich <mosemuthoni57@gmail.com>
2025-11-15 14:46:38 +0300

    creating landing url

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('developer/', include("developer.urls"))
+    path('', include("developer.urls"))
 ]
